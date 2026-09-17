b3f8156ddc162f9835caecf97c7c7ba9c1ab512f
anneapen <annmary181@gmail.com>
2026-09-17 20:16:00 +0530

    Added read_beam_file() to fea_model.py

--- fea_model.py
+++ fea_model.py
@@ -8,3 +8,17 @@ def calc_shear_modulus(nu:float, E:float) -> float:
     G = E / (2 * (1+nu))
     return G
 
+#Reading data from the text file
+def read_beam_file(filename: str) -> list[list[str]]:
+    
+    """
+    Returna a list of list of strings representing the text data in the file at
+    'filename'
+    """
+    csv_acc = [] # File data goes here
+    with open(filename, "r") as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for line in csv_reader:
+            csv_acc.append(line)
+            
+    return csv_acc
